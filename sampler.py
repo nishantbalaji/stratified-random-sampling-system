ef41ef12304d290e7a29699dba8d9b833370fe9d
Nishant Balaji <nishantb1130@gmail.com>
2021-01-26 01:34:36 -0800

    clean up

--- sampler.py
+++ sampler.py
@@ -20,94 +20,109 @@ AudioMothName = temp_list
 for bad_device in invalid_devices:
     df.drop(df[df['AudioMothCode']  == bad_device].index, inplace=True)
 
-# Remove the files with an invalid length.
+# Remove the files with an invalid length
 target_size = 46080360
 df.drop(df[df['FileSize'] < target_size].index, inplace=True)
 
-
-
-#########################################################################################################################################################
-
-''' make a list of starting hours for each audiomoth device. then count the number of unique hours in each device. divide the hours by 24 (should be 1 per hour if there are  24 hours, more if not. store remainder in a new value (x) and pick a random x number of hours to pick an extra value from) and store to (num) and go through each hour and randomly choose (num) elements to add to the list. repeat for each hour in the day, for each device'''
-
+# All the possible hours that can be shown
 all_hours = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23]
 
-output = pandas.DataFrame(columns=df.columns)
-
-
+# Store the current position on the DataFrame.
 hour_index = 0
 
+# Create a DataFrame with the same columns to store the outputs
+output = pandas.DataFrame(columns=df.columns)
+
+# Repeat the random sampling for each device
 for AudioMoth in AudioMothName:
 
+    # Create a dictionary to store the indices of the all the recordings for every hour
     hours = {}
-    start_hour = int((df['StartDateTime'].iloc[hour_index])[11:13])
-    starting_index = all_hours.index(int(start_hour))
 
-
-
+    # For all the files in this device, add it to the dictionary under the corresponding hour
     while df['AudioMothCode'].iloc[hour_index] == AudioMoth:
 
-        curr_hour_item = df['StartDateTime'].iloc[hour_index]
-        curr_hour = int(curr_hour_item[11:13])
+        # Record the current hour
+        curr_hour = int(df['StartDateTime'].iloc[hour_index][11:13])
+
+        # If the hour isn't in the dictionary yet, add it
         if curr_hour not in hours:
             hours[curr_hour] = []
 
+        # If the hour is in the dictionary, add the index of the current row to that hour's list
         if curr_hour in hours:
             hours[curr_hour].append(hour_index)
-        if hour_index == len(df) -1:
+
+        # If the hour_index is at the end of the DataFrame, stop iterating
+        if hour_index == len(df) - 1:
             break
+
+        # Go to the next row
         hour_index += 1
 
 
-
+    # Find the number of clips needed from each hour
     num_per_hour = int(len(hours) / 24)
     if num_per_hour != 0:
-        num_per_hour = int(1/num_per_hour)
+        num_per_hour = int( 1 / num_per_hour)
+
+    # Find the remaining number of clips needed
     extras = abs(len(hours) % 24)
 
-
+    # Make a list of dictionarys holding the row data to add to the output file
     rows_list = []
 
-
+    # For each hour, randomly pick the `num_per_hour` amount of
+    # samples to use and add them to row_list
     for select_hour in hours:
         for i in range(num_per_hour):
+
+            # Pick a random index in the hour's list
             random.seed()
             curr_list = hours[select_hour]
-
             rand_num = random.randint(0, len(curr_list)-1)
 
+            # Add the data of the sample at this index to the dictionary, and
+            # add the dictionary to the row_list.
             rowToAdd = {}
             for column in df.columns:
                 rowToAdd[column] = df.iloc[curr_list[rand_num]][str(column)]
-
             rows_list.append(rowToAdd)
 
+            # Remove the used sample from the list
             curr_list.remove(curr_list[rand_num]);
 
 
-
+    # If there are extra samples needed, randomly pick that many hours and
+    # take a sample from each.
     for j in range(extras):
+
+        # Pick a random hour
         random.seed()
         rand_choice = random.randint(0, len(hours)-1)
 
-        random.seed()
-
+        # Get the list of that hour
         temp = list(hours.keys())
         key = temp[rand_choice]
         curr_list = hours[key]
 
+        # Pick a random index in the hour's list
+        random.seed()
         rand_num = random.randint(0, len(curr_list)-1)
-        rowToAdd = {}
 
+        # Add the data of the sample at this index to the dictionary, and
+        # add the dictionary to the row_list.
+        rowToAdd = {}
         for column in df.columns:
             rowToAdd[column] = df.iloc[curr_list[rand_num]][str(column)]
         rows_list.append(rowToAdd)
+
+        # Remove the used sample from the list
         curr_list.remove(curr_list[rand_num]);
 
-
+    # Create a temporary DataFrame from the rows_list, and then add it to the output DataFrame
     temp = pandas.DataFrame(rows_list)
-
     output = output.append(temp, ignore_index=True)
 
-
-df.to_csv('stratified_random_sample.csv', index=False)
+# Convert the DataFrame to a .csv file
+output.to_csv('stratified_random_sample.csv', index=False)
